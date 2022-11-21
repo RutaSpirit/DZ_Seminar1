bdfaed8c3cf1fd15c49412fbd3e813c5b8223a61
Vika <vikkiblossom@gmail.com>
2022-11-21 15:49:25 +0300

    Add Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число и выдает, является ли число четным
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(Convert.ToInt32(n > 5));
+
+if (n % 2 == 0)
+    Console.Write("yes");
+else
+    Console.Write("no");
