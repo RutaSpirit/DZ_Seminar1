fb08f88d1632427ebae838d202bfe3ae2d80ce8f
Vika <vikkiblossom@gmail.com>
2022-11-21 18:03:17 +0300

    Add Task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат(число, умноженное на само себя)
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+if (m * m == n)
+    Console.Write("yes");
+else
+    Console.Write("no");
