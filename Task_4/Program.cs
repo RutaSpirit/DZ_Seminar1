994e67ac444c8e07a188be29fdde20e648ad08e7
Vika <vikkiblossom@gmail.com>
2022-11-22 10:01:25 +0300

    Changed Task 4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -7,20 +7,36 @@ int m = Convert.ToInt32(Console.ReadLine()); // ввод чисел
 Console.Write("Введите 3-е число: ");
 int l = Convert.ToInt32(Console.ReadLine());
 int max = n;
-{ if (m > n > l)
-  {
-    max = m;
-    Console.Write("max = ");
-    Console.WriteLine(max);
-  }
-  else
-  {
-    max = n;
-    Console.Write("max = ");
-    Console.WriteLine(max);
-   }
-}
-if (m == n == l)
 {
-    Console.Write("Вы ввели одинаковые числа!");
+    if (m > n && m > l)
+    {
+        max = m;
+        Console.Write("max = ");
+        Console.WriteLine(max);
+    }
+    else if (n > m && n > l)
+    {
+        max = n;
+        Console.Write("max = ");
+        Console.WriteLine(max);
+    }
+    else if (l > m && l > n)
+    {
+        max = l;
+        Console.Write("max = ");
+        Console.WriteLine(max);
+    }
+
+    else if (m == n && l > m)
+    {
+        max = l;
+        Console.Write("max = ");
+        Console.WriteLine(max);
+
+    }
+    else if (m == l && l == n)
+    {
+        Console.Write("Вы ввели одинаковые числа!");
+    }
+
 }
