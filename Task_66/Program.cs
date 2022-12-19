eab162bbc6a89d9934b582507ad4278676e9017c
Vika <vikkiblossom@gmail.com>
2022-12-19 12:56:48 +0300

    Add Task 66 and Task 68

---
+++ Task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿int rec(int m, int n)
+{
+if (m == n)
+return n;
+return rec(m, n - 1) + n;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
---
+++ Task_68/Program.cs
@@ -0,0 +1,15 @@
+﻿int rec(int n, int m)
+{
+if (n == 0)
+    return m + 1;
+else if (n != 0 && m == 0)
+    return rec(n - 1, 1);
+else
+    return rec(n - 1, rec(n, m - 1));
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
